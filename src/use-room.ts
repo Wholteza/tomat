61c5c581e2f2f6fe2888a1d2d1c1bc654c4e5a05
Zackarias Montell <zackarias@montell.se>
2022-04-08 22:52:56 +0200

    Continued on structure for shared rooms and timers

--- src/use-room.ts
+++ src/use-room.ts
@@ -1,4 +1,5 @@
-import { add, differenceInSeconds } from "date-fns";
+import { PrecisionManufacturing, TimerTwoTone } from "@mui/icons-material";
+import { add, addSeconds, differenceInSeconds } from "date-fns";
 import { FirebaseApp } from "firebase/app";
 import {
   addDoc,
@@ -9,6 +10,7 @@ import {
   DocumentData,
   Firestore,
   FirestoreDataConverter,
+  getDoc,
   getDocs,
   getFirestore,
   onSnapshot,
@@ -23,6 +25,7 @@ import { useInterval } from "usehooks-ts";
 const ROOM_NAME = "Yggdrasil";
 const USER_NAME = "Anonymous User";
 
+const ROOM_COLLECTION_PATH = "rooms";
 const USER_COLLECTION_PATH = "users";
 const TIMER_COLLECTION_PATH = "timers";
 const NON_EXISTING_TIMER = "0";
@@ -32,21 +35,92 @@ export enum TimerType {
   Break = "Break",
 }
 
-type Timer = {
-  id: string;
-  startTime: Date;
-  type: TimerType;
-  durationSeconds: number;
-};
+class Timer {
+  public id?: string;
+  public startTime: Date;
+  public endTime: Date;
+  public type: TimerType;
+  public durationSeconds: number;
 
-type User = {
-  id?: string;
-  name: string;
-};
+  public constructor(durationSeconds: number, type: TimerType) {
+    this.type = type;
+    this.durationSeconds = durationSeconds;
+    this.startTime = new Date(Date.now());
+    this.endTime = addSeconds(this.startTime, durationSeconds);
+  }
 
-type Room = {
-  timer: Timer;
-};
+  public static userConverter: FirestoreDataConverter<Timer> = {
+    toFirestore: (timer) => ({
+      startTime: timer.startTime,
+      endTime: timer.endTime,
+      type: timer.type,
+      durationSeconds: timer.durationSeconds,
+    }),
+    fromFirestore: (snapshot, options) => {
+      const data = snapshot.data(options);
+      const timer = new Timer(data.durationSeconds, data.type);
+      timer.id = data.id;
+      timer.startTime = new Date(data.startTime);
+      timer.endTime = new Date(data.endTime);
+
+      return timer;
+    },
+  };
+}
+
+class User {
+  public id?: string;
+  public name: string;
+  public lastSeen: Date;
+
+  public constructor(name: string) {
+    this.name = name;
+    this.lastSeen = new Date(Date.now());
+  }
+
+  public static firebaseConverter: FirestoreDataConverter<User> = {
+    toFirestore: (user) => ({
+      name: user.name,
+      lastSeen: user.lastSeen,
+    }),
+    fromFirestore: (snapshot, options) => {
+      const data = snapshot.data(options);
+      const user = new User(data.name);
+      user.id = data.id;
+      user.lastSeen = data.lastSeen;
+      return user;
+    },
+  };
+}
+
+class Room {
+  public id?: string;
+  public name: string;
+  public timer: Timer;
+  public users: User[];
+
+  public constructor(name: string) {
+    this.name = name;
+    this.users = [];
+    this.timer = new Timer(0, TimerType.Work);
+  }
+
+  public static firebaseConverter: FirestoreDataConverter<Room> = {
+    toFirestore: (room) => ({
+      timer: room.timer,
+      users: room.users,
+    }),
+    fromFirestore: (snapshot, options) => {
+      const data = snapshot.data(options);
+      const room = new Room(data.name);
+      room.id = data.id;
+      room.timer = data.timer;
+      room.users = data.users;
+      return room;
+    },
+  };
+}
+
 type TimeLeft = { minutes: number; seconds: number; finished: boolean };
 
 const getTimeLeft = (timer: Timer) => {
@@ -70,68 +144,31 @@ const getTimeLeft = (timer: Timer) => {
 //   }),
 //   fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as Timer,
 // });
-const userConverter: FirestoreDataConverter<User> = {
-  toFirestore: (user) => ({
-    id: user.id,
-    name: user.name,
-  }),
-  fromFirestore: (snapshot, options) => {
-    const data = snapshot.data(options);
-    return { name: data.name, id: data.id };
-  },
-};
 
 type Props = {
-  addUser: () => Promise<void>;
   timer: Timer;
   timeLeft: TimeLeft;
   startNewTimer: (durationSeconds: number, type: TimerType) => void;
 };
-const createInitialTimer = (
-  durationSeconds: number,
-  type: TimerType
-): Timer => ({
-  id: NON_EXISTING_TIMER,
-  type,
-  durationSeconds,
-  startTime: new Date(Date.now()),
-});
-
-const createTimer = (durationSeconds: number, type: TimerType): Timer => ({
-  id: (Math.random() * 100_000_000_000_000_000).toString(),
-  type,
-  durationSeconds,
-  startTime: new Date(Date.now()),
-});
 
 const useRoom = (app: FirebaseApp): Props => {
-  const [timer, setTimer] = useState<Timer>(
-    createInitialTimer(0, TimerType.Work)
-  );
+  const db = useMemo<Firestore>(() => getFirestore(app), [app]);
+  const [timer, setTimer] = useState<Timer>(new Timer(0, TimerType.Work));
   const [user, setUser] = useState<User>();
+  const [room, setRoom] = useState<Room>();
   const [timeLeft, setTimeLeft] = useState<TimeLeft>(
     (timer && getTimeLeft(timer)) || {
       minutes: 0,
       seconds: 0,
+      finished: true,
     }
   );
-  // infra
-  const db = useMemo<Firestore>(() => getFirestore(app), [app]);
-  // const timersCollectionReference = useMemo<CollectionReference<Timer>>(
-  //   () =>
-  //     collection(db, TIMER_COLLECTION_PATH).withConverter<Timer>(
-  //       timerConverter()
-  //     ),
-  //   [db]
-  // );
 
   const updateUser = useCallback(async () => {
     try {
-      const user: User = {
-        name: USER_NAME,
-      };
-      const docRef = doc(db, USER_COLLECTION_PATH, "123").withConverter(
-        userConverter
+      const user = new User(USER_NAME);
+      const docRef = doc(db, USER_COLLECTION_PATH, "123").withConverter<User>(
+        User.firebaseConverter
       );
       await setDoc(docRef, user);
       console.log("Document written with ID: ", docRef.id);
@@ -140,20 +177,65 @@ const useRoom = (app: FirebaseApp): Props => {
     }
   }, [db]);
 
-  const createUser = useCallback(async () => {
-    try {
-      const user: User = {
-        name: USER_NAME,
-      };
-      const docRef = await addDoc(collection(db, USER_COLLECTION_PATH), user);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }, [db]);
+  const createUser = useCallback(
+    async (newUser: User) => {
+      try {
+        const ref = await addDoc(
+          collection(db, USER_COLLECTION_PATH).withConverter<User>(
+            User.firebaseConverter
+          ),
+          newUser
+        );
+        setUser((prev) => {
+          if (!prev) return;
+          prev.id = ref.id;
+          return prev;
+        });
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    },
+    [db]
+  );
+
+  const getRoom = useCallback(
+    async (newRoom: Room) => {
+      try {
+        const docRef = doc(
+          db,
+          ROOM_COLLECTION_PATH,
+          newRoom.name
+        ).withConverter(Room.firebaseConverter);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setRoom(docSnap.data());
+          return;
+        }
+        const roomsRef = collection(
+          db,
+          ROOM_COLLECTION_PATH
+        ).withConverter<Room>(Room.firebaseConverter);
+
+        const ref = await addDoc(roomsRef, newRoom);
+        setRoom((prev) => {
+          if (!prev) return;
+          prev.id = ref.id;
+          return prev;
+        });
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    },
+    [db]
+  );
 
   useInterval(() => {
-    let newTime: TimeLeft = timer && getTimeLeft(timer);
-    if (newTime.finished) newTime = { ...newTime, minutes: 0, seconds: 0 };
+    let newTime = (timer && getTimeLeft(timer)) || {
+      seconds: 0,
+      minutes: 0,
+      finished: true,
+    };
+    if (newTime?.finished) newTime = { ...newTime, minutes: 0, seconds: 0 };
     setTimeLeft(newTime);
   }, 10);
 
@@ -171,7 +253,7 @@ const useRoom = (app: FirebaseApp): Props => {
 
   const startNewTimer = useCallback(
     (durationSeconds: number, type: TimerType) => {
-      const timer = createTimer(durationSeconds, type);
+      const timer = new Timer(durationSeconds, type);
       setTimer(timer);
       //replaceTimer(timer);
     },
@@ -202,8 +284,25 @@ const useRoom = (app: FirebaseApp): Props => {
   // useEffect(() => {
   //   getTimers();
   // }, [getTimers]);
+  useEffect(() => {
+    if (user?.id) return;
+    const newUser = new User(USER_NAME);
+    setUser(newUser);
+    createUser(newUser);
+  }, [createUser, user?.id]);
 
-  return { timeLeft, startNewTimer, timer, addUser: updateUser };
+  useEffect(() => {
+    console.log(0);
+    if (!user?.id) return;
+    console.log(1);
+    if (room) return;
+    console.log("adding room");
+    const newRoom = new Room(ROOM_NAME);
+    setRoom(newRoom);
+    getRoom(newRoom);
+  }, [getRoom, room, user?.id]);
+
+  return { timeLeft, startNewTimer, timer };
 };
 
 export default useRoom;
