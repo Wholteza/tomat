efb63127a7c83095c6dc10c4898a30bcd0e5a8b1
Zackarias Montell <zackarias@montell.se>
2022-04-12 21:00:20 +0200

    moved production values

--- src/infrastructure/firebase/use-firebase.ts
+++ src/infrastructure/firebase/use-firebase.ts
@@ -8,13 +8,13 @@ type UseFirebaseProps = {
 };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBF9v-SACLjTGx7LtqbibemyiDSK53O6oY",
-  authDomain: "tomat-d4561.firebaseapp.com",
-  projectId: "tomat-d4561",
-  storageBucket: "tomat-d4561.appspot.com",
-  messagingSenderId: "543488032716",
-  appId: "1:543488032716:web:8f8863fedcc592bfab2769",
-  measurementId: "G-73RWN6THGC",
+  apiKey: import.meta.env.firebaseConfigApiKey,
+  authDomain: import.meta.env.firebaseConfigAuthDomain,
+  projectId: import.meta.env.firebaseConfigProjectId,
+  storageBucket: import.meta.env.firebaseConfigStorageBucket,
+  messagingSenderId: import.meta.env.firebaseConfigMessagingSenderId,
+  appId: import.meta.env.firebaseConfigAppId,
+  measurementId: import.meta.env.firebaseConfigMeasurementId,
 };
 
 const useFirebase = (): UseFirebaseProps => {
